d290f86246d3724cbad50334952be94a1db8931a
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-07-06 23:57:29 +0000

    chore: remove temporary scratchpad scripts

--- patch_re.py
+++
@@ -1,8 +0,0 @@
-with open("scanner/cli/appguardrail.py", "r") as f:
-    code = f.read()
-
-code = code.replace("    import re\n", "")
-code = code.replace("    import os\n", "")
-
-with open("scanner/cli/appguardrail.py", "w") as f:
-    f.write(code)
