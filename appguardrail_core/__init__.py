a20d322a107ed8eadeb9a7f4c46e750b2501ff4a
Seongho Bae <me@seonghobae.me>
2026-08-04 15:26:04 +0900

    feat(openssf): export public evidence API

--- appguardrail_core/__init__.py
+++ appguardrail_core/__init__.py
@@ -9,54 +9,82 @@ from appguardrail_core.code_scanning import (
     compare_snapshots as compare_code_scanning_snapshots,
     normalize_analysis as normalize_code_scanning_analysis,
 )
-from appguardrail_core.external import (ExternalEngineDecision,
-                                        ExternalScanPlan,
-                                        build_external_scan_plan)
-from appguardrail_core.findings import (DEPLOY_BLOCKING_SEVERITIES,
-                                        NON_BLOCKING_CONTEXTS, SEVERITIES,
-                                        finding_sort_key, is_deploy_blocking,
-                                        normalize_finding, normalize_findings,
-                                        safe_report_snippet, severity_counts)
-from appguardrail_core.language import (StackProfile, detect_language_axes,
-                                        detect_stack_profile)
-from appguardrail_core.metrics import (MetricResult, SaleReadinessInputs,
-                                       SaleReadinessScore,
-                                       score_sale_readiness)
-from appguardrail_core.org_intelligence import (BuyerEvidenceMetric,
-                                                BuyerEvidencePack,
-                                                OrgInventory,
-                                                PullRequestGateSummary,
-                                                RepositoryGateSummary,
-                                                build_buyer_evidence_pack,
-                                                build_org_inventory,
-                                                buyer_evidence_pack_to_dict,
-                                                classify_pr_gate,
-                                                gate_action_bucket,
-                                                render_org_readiness_report,
-                                                summarize_pr_gates)
-from appguardrail_core.reports import (ReportContext,
-                                       render_buyer_diligence_report)
-from appguardrail_core.rules import (RuleMetadata, build_rule_metadata,
-                                     extract_public_references,
-                                     validate_rule_metadata)
+from appguardrail_core.external import (
+    ExternalEngineDecision,
+    ExternalScanPlan,
+    build_external_scan_plan,
+)
+from appguardrail_core.findings import (
+    DEPLOY_BLOCKING_SEVERITIES,
+    NON_BLOCKING_CONTEXTS,
+    SEVERITIES,
+    finding_sort_key,
+    is_deploy_blocking,
+    normalize_finding,
+    normalize_findings,
+    safe_report_snippet,
+    severity_counts,
+)
+from appguardrail_core.language import (
+    StackProfile,
+    detect_language_axes,
+    detect_stack_profile,
+)
+from appguardrail_core.metrics import (
+    MetricResult,
+    SaleReadinessInputs,
+    SaleReadinessScore,
+    score_sale_readiness,
+)
+from appguardrail_core.openssf_evidence import (
+    OpenSSFEvidence,
+    collect_openssf_evidence,
+    evidence_to_finding,
+    parse_project_matches as parse_openssf_project_matches,
+)
+from appguardrail_core.org_intelligence import (
+    BuyerEvidenceMetric,
+    BuyerEvidencePack,
+    OrgInventory,
+    PullRequestGateSummary,
+    RepositoryGateSummary,
+    build_buyer_evidence_pack,
+    build_org_inventory,
+    buyer_evidence_pack_to_dict,
+    classify_pr_gate,
+    gate_action_bucket,
+    render_org_readiness_report,
+    summarize_pr_gates,
+)
+from appguardrail_core.reports import (
+    ReportContext,
+    render_buyer_diligence_report,
+)
+from appguardrail_core.rules import (
+    RuleMetadata,
+    build_rule_metadata,
+    extract_public_references,
+    validate_rule_metadata,
+)
 
 __all__ = [
     "AnalysisEvidence",
     "AnalysisIdentity",
     "AnalysisSnapshot",
+    "BuyerEvidenceMetric",
+    "BuyerEvidencePack",
     "DEPLOY_BLOCKING_SEVERITIES",
     "DriftAssessment",
     "ExternalEngineDecision",
     "ExternalScanPlan",
-    "BuyerEvidenceMetric",
-    "BuyerEvidencePack",
-    "ReportContext",
-    "RuleMetadata",
     "MetricResult",
     "NON_BLOCKING_CONTEXTS",
+    "OpenSSFEvidence",
     "OrgInventory",
     "PullRequestGateSummary",
+    "ReportContext",
     "RepositoryGateSummary",
+    "RuleMetadata",
     "SEVERITIES",
     "SaleReadinessInputs",
     "SaleReadinessScore",
@@ -68,9 +96,11 @@ __all__ = [
     "build_rule_metadata",
     "buyer_evidence_pack_to_dict",
     "classify_pr_gate",
+    "collect_openssf_evidence",
     "compare_code_scanning_snapshots",
     "detect_language_axes",
     "detect_stack_profile",
+    "evidence_to_finding",
     "extract_public_references",
     "finding_sort_key",
     "gate_action_bucket",
@@ -78,8 +108,9 @@ __all__ = [
     "normalize_code_scanning_analysis",
     "normalize_finding",
     "normalize_findings",
-    "render_org_readiness_report",
+    "parse_openssf_project_matches",
     "render_buyer_diligence_report",
+    "render_org_readiness_report",
     "safe_report_snippet",
     "score_sale_readiness",
     "severity_counts",
